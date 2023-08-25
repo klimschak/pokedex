820521dcb89388b59d3022a1fb155a102757149e
Emanuel Klimschak <klimschak@gmail.com>
2023-08-25 20:50:58 +0200

    tabs ready

--- script.js
+++ script.js
@@ -10,23 +10,40 @@ let chainEvolution1;
 let chainEvolution1Img;
 let chainEvolution2;
 let chainEvolution2Img;
-let pokeName = "charizard";
+let pokeName = "bulbasaur";
 let pokeUrl;
 
 let pokeData;
 
 async function init() {
-  await loadApiInfo(pokeName);
+  
   await loadApiCharacteristics();
   await loadApiEvolution()
-  renderAbout()
-  renderPokemonInfo()
+  await loadApiInfo(pokeName);
   renderCardHero()
-  renderStats ()
-  loadPokemonList()
+  renderAbout()
+  scrollToTop() 
+ 
+ 
+  //loadPokemonList()
 
 
 
+}
+
+function setPokeName (newPokemon){
+  
+  pokeName = newPokemon;
+  init()
+
+}
+
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' // Verwende 'auto' für sofortiges Scrollen
+  });
 }
 
 async function loadPokemonList() {
@@ -35,11 +52,12 @@ async function loadPokemonList() {
 
 }
 
-async function loadApiInfo(pokeName) {
-  
-  let pokeUrl = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
+async function loadApiInfo(pokemon) {
+
+  let pokeUrl = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
   let pokeResponse = await fetch(pokeUrl);
   currentPokemon = await pokeResponse.json();
+ 
 
 }
 
@@ -60,6 +78,69 @@ async function loadApiCharacteristics() {
   let characteristicUrl = "https://pokeapi.co/api/v2/characteristic/1";
   const characteristicResponse = await fetch(characteristicUrl);
   currentCharacteristic = await characteristicResponse.json();
+}
+
+
+
+function getCharacteristicDescription() {
+  for (let i = 0; i < currentCharacteristic.descriptions.length; i++) {
+    if (currentCharacteristic.descriptions[i].language.name === "en") {
+      let currentCharacteristicEN =
+        currentCharacteristic.descriptions[i].description;
+      return currentCharacteristicEN;
+      break; // Beende die Schleife, sobald die englische Übersetzung gefunden wurde
+    }
+  }
+}
+
+////////// CARD-HERO ////////////////
+
+function renderCardHero() {
+  pokeName = currentPokemon.name;
+  let pokeID = currentPokemon.id;
+  let pokeImgLarge = currentPokemon.sprites.other.dream_world.front_default;
+  document.getElementById("card-hero").innerHTML = /*html*/`
+    <img src="${pokeImgLarge}" alt="" id="card-hero-image">
+    <div id="right-card-hero">
+      <p id="hero-id">#00${pokeID}</p>
+      <h2 id="poke-name">${pokeName}</h2>
+      <div id="type-button-container"></div>
+
+    </div>
+  `
+  for (let i = 0; i < currentPokemon.types.length; i++) {
+    let pokeType = currentPokemon.types[i].type.name;
+    document.getElementById("type-button-container").innerHTML += /*html*/`
+    <button class="type-button button-${pokeType}"><img class="icon-type" src="/img/${pokeType}-icon.svg" alt=""><span>${pokeType}</span></button>
+  `
+  }
+}
+
+////////// ABOUT-Section ////////////////
+function renderAbout() {
+  let pokeWeight = currentPokemon.weight;
+  let pokeHeight = currentPokemon.height;
+  let pokeBaseExp = currentPokemon.base_experience;
+  let characteristicDescription = getCharacteristicDescription();
+  document.getElementById("card-content").innerHTML = /*html*/`
+  <div class="poke-description">${characteristicDescription}</div>
+    <h3>Overview</h3>
+    <div class="overview">
+      <div class="row-about"><div class="left-row-about">Weight:</div><div class="right-row-about">${pokeWeight}</div></div>
+      <div class="row-about"><div class="left-row-about">height:</div><div class="right-row-about">${pokeHeight}</div></div>
+      <div class="row-about"><div class="left-row-about">BaseEXP:</div><div class="right-row-about">${pokeBaseExp}</div></div>
+    </div>
+  `
+
+    getEvolutions();
+    document.getElementById("card-content").innerHTML += /*html*/`
+    <h3>Evolutions</h3>
+    <div id="evolutions">
+        <div class="row-evolution"><div class="left-row-evolution" onclick="setPokeName('${chainEvolution0}')"><img class="evo-image" src="${chainEvolution0Img}"></div><div class="right-row-evolution">${chainEvolution0}</div></div>
+        <div class="row-evolution"><div class="left-row-evolution" onclick="setPokeName('${chainEvolution1}')"><img class="evo-image" src="${chainEvolution1Img}"></div><div class="right-row-evolution">${chainEvolution1}</div></div>
+        <div class="row-evolution"><div class="left-row-evolution" onclick="setPokeName('${chainEvolution2}')"><img class="evo-image" src="${chainEvolution2Img}"></div><div class="right-row-evolution">${chainEvolution2}</div></div>
+    </div>
+`
 }
 
 async function loadApiEvolution() {
@@ -84,131 +165,21 @@ async function loadApiEvolution() {
   //chainEvolutionLink2 = currentEvolution.chain.evolves_to[0].evolves_to[0].species.url;
   chainEvolution2Img = await loadApiInfoParam(chainEvolution2);
   chainEvolution2Img = chainEvolution2Img.sprites.other.dream_world.front_default;
-
-
-
-
 }
-
-
-
-
-function renderPokemonInfo() {
-  // Allgemeine Informationen
-  pokeName = currentPokemon.name;
-
-  let pokeImgDefault = currentPokemon.sprites.front_default;
-  let pokeImgShiny = currentPokemon.sprites.front_shiny;
-  let pokeImgLarge = currentPokemon.sprites.other.dream_world.front_default;
-  //let pokeEvolution = currentEvolution.chain.evolves_to[0].species.name  ;
-
-
-  let characteristicDescription = getCharacteristicDescription();
-  
-
-
-
-  // Fähigkeiten
-
-
-
-
-
-
-  document.getElementById("info-list").innerHTML += `<div><img src="${pokeImgDefault}"></img></div>`;
-  document.getElementById("info-list").innerHTML += `<div><img src="${pokeImgShiny}"></img></div>`;
-
-  document.getElementById("info-list").innerHTML += `<div><div>Description:</div> <div>${characteristicDescription}</div></div>`;
-  
-
-  
-
-
-  displayAbilities();
-
-}
-
-
-
 
 
 function getEvolutions() {
-
   for (let i = 0; i < currentEvolution.chain.evolves_to.length; i++) {
     let pokeEvolution = currentEvolution.chain.evolves_to[i].species.name;
     return pokeEvolution;
   }
-
-
 }
 
-function getCharacteristicDescription() {
-  for (let i = 0; i < currentCharacteristic.descriptions.length; i++) {
-    if (currentCharacteristic.descriptions[i].language.name === "en") {
-      let currentCharacteristicEN =
-        currentCharacteristic.descriptions[i].description;
-      return currentCharacteristicEN;
-      break; // Beende die Schleife, sobald die englische Übersetzung gefunden wurde
-    }
-  }
-}
-
-////////// CARD-HERO ////////////////
-
-function renderCardHero() {
-  pokeName = currentPokemon.name;
-  let pokeID = currentPokemon.id;
-  let pokeImgLarge = currentPokemon.sprites.other.dream_world.front_default;
-  document.getElementById("card-hero").innerHTML = /*html*/`
-    <img src="${pokeImgLarge}" alt="" id="card-hero-image">
-    <div id="right-card-hero">
-                  <p id="hero-id">#00${pokeID}</p>
-                  <h2 id="poke-name">${pokeName}</h2>
-                  <div id="type-button-container"></div>
-
-    </div>
-  `
-  for (let i = 0; i < currentPokemon.types.length; i++) {
-    let pokeType = currentPokemon.types[i].type.name;
-    document.getElementById("type-button-container").innerHTML += /*html*/`
-    <button class="type-button button-${pokeType}"><img class="icon-type" src="/img/${pokeType}-icon.svg" alt=""><span>${pokeType}</span></button>
-  `
-  }
-
-}
-
-////////// ABOUT-Section ////////////////
-function renderAbout () {
-  let pokeWeight = currentPokemon.weight;
-  let pokeHeight = currentPokemon.height;
-  let pokeBaseExp = currentPokemon.base_experience;
-  document.getElementById("card-content").innerHTML = /*html*/`
-    <h3>Overview</h3>
-    <div class="overview">
-      <div class="row-about"><div class="left-row-about">Weight:</div><div class="right-row-about">${pokeWeight}</div></div>
-      <div class="row-about"><div class="left-row-about">height:</div><div class="right-row-about">${pokeHeight}</div></div>
-      <div class="row-about"><div class="left-row-about">BaseEXP:</div><div class="right-row-about">${pokeBaseExp}</div></div>
-
-      
-
-    </div>
-  `
-
-getEvolutions();
-document.getElementById("card-content").innerHTML += /*html*/`
-  <h3>Evolutions</h3>
-  <div id="evolutions">
-      <div class="row-evolution"><div class="left-row-evolution"><img class="evo-image" src="${chainEvolution0Img}"></div><div class="right-row-evolution">${chainEvolution0}</div></div>
-      <div class="row-evolution"><div class="left-row-evolution"><img class="evo-image" src="${chainEvolution1Img}"></div><div class="right-row-evolution">${chainEvolution1}</div></div>
-      <div class="row-evolution"><div class="left-row-evolution"><img class="evo-image" src="${chainEvolution2Img}"></div><div class="right-row-evolution">${chainEvolution2}</div></div>
 
 
-  </div>
-`
+////////// Stats-Section ////////////////
 
-}
-
-function renderStats () {
+function renderStats() {
   document.getElementById("card-content").innerHTML = /*html*/`
     <h3>Statistics</h3>
     <div id="poke-stat-container"></div>
@@ -223,16 +194,20 @@ function renderStats () {
 
 }
 
-
-async function displayAbilities() {
+////////// Abilities-Section ////////////////
+async function renderAbilities() {
+  document.getElementById("card-content").innerHTML = /*html*/`
+  <h3>Abilities</h3>
+  <div id="abilities-container"></div>
+  `
   for (let i = 0; i < currentPokemon.abilities.length; i++) {
     let pokeAbility = currentPokemon.abilities[i].ability.name;
     let pokeAbilityEffect = await loadAbility(pokeAbility);
-
-    
-
-    document.getElementById("info-list").innerHTML += `<div><div>Ability:</div> <div>${pokeAbility}</div></div>`;
-    document.getElementById("info-list").innerHTML += `<div><div>Ability:</div> <div>${pokeAbilityEffect}</div></div>`;
+    document.getElementById("abilities-container").innerHTML += /*html*/`
+      <div class="poke-ability">
+      <h4>${pokeAbility}</h4>
+      <p>${pokeAbilityEffect}</p>
+    `
   }
 }
 
